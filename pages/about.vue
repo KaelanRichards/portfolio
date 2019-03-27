65533e5f52a844b0e24fe5c83eb355d63809995e
kadokaelan@gmail.com <kadokaelan@gmail.com>
2019-03-26 21:16:03 -0400

    first init

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is Kaelan Richards (AKA Kado), I’m a 
+        software developer based in Detroit, Michigan. 
+        I build modern applications for the web!
+        I'm a Creator, I enjoy desgining, developing, writing - creating.
+      </p>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Kaelan Richards</h1>
+      <h2 class="subtitle">a Software Developer based in Detroit, Michigan.</h2>
+    </div>
+  </section>
+</template>
