15e752a2d53329fac01e960dcd2ecf5d7985a809
kadokaelan@gmail.com <kadokaelan@gmail.com>
2019-03-26 21:41:54 -0400

    created about me page

--- pages/about.vue
+++ pages/about.vue
@@ -2,12 +2,23 @@
   <section class="section is-medium">
     <div class="container has-text-centered">
       <h1 class="title">About Me</h1>
-      <p>
-        My name is Kaelan Richards (AKA Kado), I’m a 
+      <h1>
+        My name is Kaelan
+        (AKA Kado), I’m a 
         software developer based in Detroit, Michigan. 
         I build modern applications for the web!
-        I'm a Creator, I enjoy desgining, developing, writing - creating.
-      </p>
+        I'm a Creator, I enjoy designing, developing, writing - creating.
+      </h1>
     </div>
   </section>
 </template>
+<style>
+.name-link {
+    color: #555555;
+    display: inline;
+    border-bottom: 4px dashed #999999;
+    text-decoration: none;
+    transition: ease 0.5s;
+    font-size: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,26 @@
 <template>
   <section class="section is-large">
-    <div class="container has-text-centered">
-      <h1 class="title">Hi, I'm Kaelan Richards</h1>
-      <h2 class="subtitle">a Software Developer based in Detroit, Michigan.</h2>
+    <div class="container has-text-left">
+      <h1 class="title">Hi, I'm 
+        <a href="/about" class="name-link">
+          Kaelan
+        </a> 
+      </h1>
+      <h2 class="subtitle need-padding">a Software Developer based in Detroit, Michigan.</h2>
     </div>
   </section>
 </template>
+<style>
+.name-link {
+    color: #555555;
+    display: inline;
+    border-bottom: 4px dashed #999999;
+    text-decoration: none;
+    transition: ease 0.5s;
+
+}
+
+.need-padding {
+  padding: 10px;
+}
+</style>
